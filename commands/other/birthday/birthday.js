f94e3decf5bb478e16c40889b549f2587196ff6b
Dominic C <45153623+DominicChm@users.noreply.github.com>
2021-06-17 13:48:14 -0700

    Birthday Command Formatting

--- commands/other/birthday/birthday.js
+++ commands/other/birthday/birthday.js
@@ -1,8 +1,7 @@
 const birthdayMessage = require("./messages/birthday.message.js")
 
 module.exports = async function (message, {user}) {
-  await message.say(birthdayMessage(user));
-
+    await message.say(birthdayMessage(user));
 }
 
 
--- commands/other/bunny/bunny.js
+++ commands/other/bunny/bunny.js
@@ -4,6 +4,7 @@ const bunnyImages = require("./bunnyurls.json")
 module.exports = async function (message) {
     const randomBunnyUrl = bunnyImages[Math.floor(Math.random() * bunnyImages.length)]
     await message.say(bunnyMessage(randomBunnyUrl));
+    //    await randomBunnyUrl |> bunnyMessage |> message.say; // ME WANT :D
 }
 
 
