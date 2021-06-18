8b326040dad1acf0c3c9e9243fa1f34d92da7f94
Dominic C <45153623+DominicChm@users.noreply.github.com>
2021-06-18 11:27:46 -0700

    Support async/await for module loading

--- index.js
+++ index.js
@@ -6,24 +6,28 @@ if (!fs.existsSync("./config.json"))
     throw new Error("Could not find >config.json<! Make sure you've renamed >config.json.example< to >config.json< and filled out its required fields!");
 
 //================ LOAD ================//
-const LC = global.LyraCore = {};
+(async () => {
+    const LC = global.LyraCore = {};
 
-const config = require("./config.json");
+    const config = require("./config.json");
 
-LC.logger = require("./LyraCore/logging");
-LC.baseEmbed = require("./LyraCore/BaseEmbed");
-LC.buildCommand = require("./LyraCore/buildCommand");
+    LC.logger = require("./LyraCore/logging");
+    LC.baseEmbed = require("./LyraCore/BaseEmbed");
+    LC.buildCommand = require("./LyraCore/buildCommand");
 
-const Database = require("./LyraCore/Database")();
-const CommandoClient = require("./LyraCore/CommandoClient")(config);
+    const Database = await require("./LyraCore/Database")(config);
+    const CommandoClient = await require("./LyraCore/CommandoClient")(config);
 
-const pluginAPI = {
-    config,
-    Database,
-    CommandoClient
-}
+    const pluginAPI = {
+        config,
+        Database,
+        CommandoClient
+    }
 
-LC.plugins = require("./LyraCore/plugins")(pluginAPI);
+    LC.plugins = await require("./LyraCore/plugins")(pluginAPI);
+
+    console.log("=========LOAD COMPLETE===========");
+})()
 
 
 
