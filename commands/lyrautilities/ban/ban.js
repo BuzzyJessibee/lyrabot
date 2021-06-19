192d624a9afb9e14fdf95527484dc8d1b8f87547
Dominic C <45153623+DominicChm@users.noreply.github.com>
2021-06-18 20:59:44 -0700

    Restructure ban command

--- commands/lyrautilities/ban/ban.js
+++ commands/lyrautilities/ban/ban.js
@@ -1,21 +1,25 @@
-const banMessage = require("./messages/ban.message")
+const banMessage = require("./messages/ban.message");
+const invalidUserMessage = require("./messages/invalidUser.message");
+const banErrorMessage = require("./messages/invalidUser.message");
 
-module.exports = async function (message, { userToBan, reason, daysDelete }) {
-  const extractNumber = /\d+/g;
-  const userToBanID = userToBan.match(extractNumber)[0];
-  const user = message.mentions.members.first() || (await message.guild.members.fetch(userToBanID));
-  if (user == undefined)
-    return message.channel.send(':x: Please try again with a valid user.');
-  await user.send("https://tenor.com/view/when-your-team-too-good-ban-salt-bae-gif-7580925")
-  user
-    .ban({ days: daysDelete, reason: reason })
-    .then(() => {
-      message.say(banMessage(userToBan, reason));
-    })
-    .catch(err => {
-      message.say(
-        ':x: Something went wrong when trying to ban this user, I probably do not have the permission to ban him!'
-      );
-      return console.error(err);
-    });
+const log = LyraCore.logger("Command/Ban");
+
+module.exports = async function (message, {userToBan, reason, daysDelete}) {
+    const extractNumber = /\d+/g;
+    const userToBanID = userToBan.match(extractNumber)[0];
+    const user = message.mentions.members.first() || (await message.guild.members.fetch(userToBanID));
+
+    if (!user)
+        return await message.channel.send(invalidUserMessage());
+
+    if (!user.bannable) {
+        await message.say(banErrorMessage());
+        log(`Couldn't ban user >${user.user.username}<!`, "error");
+    }
+
+    await user.ban({days: daysDelete, reason: reason});
+    await user.send("https://tenor.com/view/when-your-team-too-good-ban-salt-bae-gif-7580925");
+    await message.say(banMessage(userToBan, reason));
+
+
 };
---
+++ commands/lyrautilities/ban/messages/banError.message.js
@@ -0,0 +1,3 @@
+module.exports = function () {
+    return ':x: Something went wrong when trying to ban this user, I probably do not have the permission to ban them!'
+}
