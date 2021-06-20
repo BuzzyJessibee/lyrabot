7c075311a2fc971aacf07b87fd0fcbb10df12a95
LyranaliaRose <49761691+LyranaliaRose@users.noreply.github.com>
2021-06-19 20:51:53 -0600

    Fixed the prune bug
    Fixed that bug I mentioned in the last commit :)

--- commands/lyrautilities/prune/prune.js
+++ commands/lyrautilities/prune/prune.js
@@ -2,12 +2,12 @@ const pruneMessage = require("./messages/prune.message");
 const pruneErrorMessage = require("./messages/pruneError.message");
 
 module.exports = async function (message, {deleteCount}) {
-
-  message.channel
-    .bulkDelete(deleteCount)
-    .then(message.say(pruneMessage(deleteCount)))
-    .catch(e => {
-      console.error(e);
-      return message.say(pruneErrorMessage());
-    });
+  try {
+    await message.channel.bulkDelete(deleteCount)
+    await message.say(pruneMessage(deleteCount))
+  }
+  catch(e) {
+    console.error(e);
+    await message.say(pruneErrorMessage());
+  }
 };
